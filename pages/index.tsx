4076f2c5568020b15b8048aef1d4bb29965e9c76
Nick <nickh3592@gmail.com>
2022-12-21 09:48:43 -0500

    part 2 completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
+import { useMachine } from "@xstate/react"
+import { myMachine } from "../machine/myFirstMachine"
 
 export default function Home() {
+  const [state, send] = useMachine(myMachine)
   return (
     <div>
-      hello world
+      {
+        JSON.stringify(state.value)
+      }
+      <button onClick={() => send('MOUSEOVER')}>Mouse over</button>
+      <button onClick={() => send('MOUSEOUT')}>mouse out</button>
     </div>
     )
 }
